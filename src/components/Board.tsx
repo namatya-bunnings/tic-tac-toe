a4decfaa304fbe8d5067e1e1de70aac09f2d68ed
Niraj Amatya <namatya@bunnings.com.au>
2024-03-19 20:27:25 +1100

    setting the value of square when clicking on it

--- src/components/Board.tsx
+++ src/components/Board.tsx
@@ -3,23 +3,36 @@ import SquareBox from "./SquareBox";
 import "./Board.css";
 
 const Board = () => {
+  const [squareValues, setSquareValues] = React.useState<string[] | null[]>(
+    Array(9).fill(null)
+  );
+
+  const handleClick = (i: number) => {
+    const newArrayValue = [...squareValues];
+    if (newArrayValue[i]) {
+      return;
+    }
+    newArrayValue[i] = "X";
+    setSquareValues(newArrayValue as string[] | null[]);
+  };
+
   return (
     <div className="container">
       <h2>Tic-Tac-Toe</h2>
       <div className="row">
-        <SquareBox />
-        <SquareBox value={squareValues[1]} />
-        <SquareBox value={squareValues[2]} />
+        <SquareBox value={squareValues[0]} handleClick={() => handleClick(0)} />
+        <SquareBox value={squareValues[1]} handleClick={() => handleClick(1)} />
+        <SquareBox value={squareValues[2]} handleClick={() => handleClick(2)} />
       </div>
       <div className="row">
-        <SquareBox value={squareValues[3]} />
-        <SquareBox value={squareValues[4]} />
-        <SquareBox value={squareValues[5]} />
+        <SquareBox value={squareValues[3]} handleClick={() => handleClick(3)} />
+        <SquareBox value={squareValues[4]} handleClick={() => handleClick(4)} />
+        <SquareBox value={squareValues[5]} handleClick={() => handleClick(5)} />
       </div>
       <div className="row">
-        <SquareBox value={squareValues[6]} />
-        <SquareBox value={squareValues[7]} />
-        <SquareBox value={squareValues[8]} />
+        <SquareBox value={squareValues[6]} handleClick={() => handleClick(6)} />
+        <SquareBox value={squareValues[7]} handleClick={() => handleClick(7)} />
+        <SquareBox value={squareValues[8]} handleClick={() => handleClick(8)} />
       </div>
     </div>
   );
