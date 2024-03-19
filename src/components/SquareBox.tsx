b1e2e730b6a9ffeb32e6282fc7f7028df1b92483
Niraj Amatya <namatya@bunnings.com.au>
2024-03-19 20:30:32 +1100

    types for the props and using onClick even handler for the button

--- src/components/SquareBox.tsx
+++ src/components/SquareBox.tsx
@@ -1,8 +1,17 @@
 import React from "react";
 import "./SquareBox.css";
 
-const SquareBox = () => {
-  return <button className="square-box"></button>;
+type SquareBoxTypes = {
+  value: string | null;
+  handleClick: () => void;
+};
+
+const SquareBox = ({ value, handleClick }: SquareBoxTypes) => {
+  return (
+    <button className="square-box" onClick={handleClick}>
+      {value}
+    </button>
+  );
 };
 
 export default SquareBox;
